d86a198766c74dff4f2908376cbe436ee7d64f03
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-07-01 11:18:22 +0200

    Add index.html and server.js from chat gpt

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
